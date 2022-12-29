9068bc74d859eed207668fe0cf22e2d5444a87e9
이주현 <joohyun333@diquest.com>
2022-12-29 17:28:46 +0900

    시작

--- leejoohyun/springbatch/src/main/java/com/example/springbatch/SpringbatchApplication.java
+++ leejoohyun/springbatch/src/main/java/com/example/springbatch/SpringbatchApplication.java
@@ -1,9 +1,12 @@
 package com.example.springbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing
 public class SpringbatchApplication {
 
 	public static void main(String[] args) {
