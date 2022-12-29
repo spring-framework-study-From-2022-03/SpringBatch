163655786b1b401d2987f4b584bdc8537236632c
DESKTOP-41K4BMT\DIQUEST <joohyun333@diquest.com>
2022-12-29 15:50:29 +0900

    다 까먹어서 처음부터 다시하기

--- leejoohyun/springbatch/springbatch/src/test/java/com/example/springbatch/SpringbatchApplicationTests.java
+++ leejoohyun/springbatch/springbatch/src/test/java/com/example/springbatch/SpringbatchApplicationTests.java
@@ -1,4 +1,4 @@
-package io.springbatch.springbatchlecture;
+package com.example.springbatch;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbatchApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
